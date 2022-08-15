2500c73cdcb4fbaa93a6a690f8374a86ec62a0fb
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-08-15 12:07:19 -0500

    wrote index.js, errors present

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,39 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_product'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_tag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'tag'
+})
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+})
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+})
+Product.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+})
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
